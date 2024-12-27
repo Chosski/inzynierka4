687288db92f9c30106e4b10e9c4308733830cd3a
Chosski <132196621+Chosski@users.noreply.github.com>
2024-12-27 08:00:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const PORT = process.env.PORT || 3000;
  * np. DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
  */
 const dbConfig = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT || 3306,
+  user: process.env.MYSQLUSER,
+  password: process.env.PASSWORD,
+  database: process.env.MYSQLDATABASE,
 };
 
 /**
